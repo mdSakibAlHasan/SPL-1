c18273aeaf54859c4d41e3830481b976da34c908
mdSakibAlHasan <76566629+mdSakibAlHasan@users.noreply.github.com>
2022-03-05 23:21:52 +0600

    add wellcome page

--- main.cpp
+++ main.cpp
@@ -5,32 +5,42 @@ using namespace std;
 
 void linear_algebra_calculation()           //add infinite many solution
 {
+    cout<<"\n\n\n WELCOME TO EQUATON SOLVER\n\n\n";
     int choice;
-
-    cout<<"1. Input equation\n 2.Input coefficient of equation"<<endl;
-    cout<<"Input your choice: ";
+    cout<<"\t\t1.Linear algebra\n\t\t2.Linear programming\n\n..........";
     cin>>choice;
 
     if(choice == 1){
-        solve_linear_equation();
+        cout<<"\n\n\n\n\t\t1. Input equation\n\t\t 2.Input coefficient of equation\n.........."<<endl;
+
+        cin>>choice;
+
+        if(choice == 1){
+            solve_linear_equation();
+        }
+        else if(choice == 2){
+            create_matrix();
+        }
+        else{
+            cout<<"Wrong input"<<endl;
+            linear_algebra_calculation();
+        }
     }
     else if(choice == 2){
-        create_matrix();
+        input_linear_programming();
     }
-    else{
-        cout<<"Wrong input"<<endl;
-        linear_algebra_calculation();
-    }
+
+
 }
 
 
 
 int main()
 {
-    freopen("input_file.txt","r",stdin);
-    input_linear_programming();
+    //freopen("input_file.txt","r",stdin);
+
 
     //freopen("C:\\Users\\HP\\Desktop\\input_SPL.txt","r",stdin);
-    //linear_algebra_calculation();
+    linear_algebra_calculation();
     return 0;
 }
